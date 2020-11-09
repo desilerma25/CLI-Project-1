ccaec7a62b3882fcaac9abac82ab58504273306e
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-09 13:29:06 -0600

    require Bundler to install/run all Gems I list

--- environment.rb
+++ environment.rb
@@ -2,6 +2,9 @@
 
 #./ means look at the level right above me 
 
+require 'bundler' # allows to run package to ensure everything in your is installed and require all gems listed 
+Bundler.require # req all gems listed in Gemfile
+
 require_relative "./lib/api"
 require_relative "./lib/cli"
 require_relative "./lib/project"
